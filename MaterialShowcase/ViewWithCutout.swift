6ec77c7c729ffa145b2c4c6e5efecdbd5cab228d
petemillermultimedia <pete@petemiller-multimedia.com>
2017-07-30 19:34:01 -0400

    Update and rename PartialTransparentMaskView.swift to ViewWithCutout.swift

---
+++ MaterialShowcase/ViewWithCutout.swift
@@ -0,0 +1,40 @@
+import Foundation
+import UIKit
+
+class ViewWithCutout: UIView {
+
+    var _cutout: CGRect?
+
+    // MARK: - Initialization
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    init(frame: CGRect, cutout: CGRect?) {
+        super.init(frame: frame)
+        _cutout = cutout
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        if self._cutout != nil {
+            // Ensures to use the current background color to set the filling color
+            self.backgroundColor?.setFill()
+            UIRectFill(rect)
+
+            let layer = CAShapeLayer()
+            let path = CGMutablePath()
+
+            // Make hole in view's overlay
+            // NOTE: Here, instead of using the transparentHoleView UIView we could use a specific CFRect location instead...
+            path.addEllipse(in: _cutout!)
+            path.addRect(bounds)
+
+            layer.path = path
+            layer.fillRule = kCAFillRuleEvenOdd
+            self.layer.mask = layer
+        }
+    }
+}
